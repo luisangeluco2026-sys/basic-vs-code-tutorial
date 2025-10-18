a2b1dd3a1003b9a9a2bb7dfe17a34727f1d16a3b
Luis Angel <luis.angel.uco.2026@gmail.com>
2025-10-17 19:34:00 -0500

    no sé jajjaja

---
+++ documento_de_practica_borrar.py
@@ -0,0 +1,13 @@
+#save=cntrl + s=guarda
+print("alt + arriba o baja=mueve la linea de codigo para arriba o abajo")
+print("control + izquierda o derecha=  la linea de escribir se mueve palabra por palabra")
+print("shift + izquierda o derecha =resaltar lo que escribes")
+print("tab = espacio identado ya configurado para meter")
+print("shift + tab = elimina ese espacio de identacion ya configurado")
+# print("cntrl + } =toda la linea se hace comentario")
+# print("hola undoeeeeeeeeeeeeeeeeeee") 
+print("control + p = buscador general de archivos trabajados aqui")
+print("control + shift + p =muestra toda la tabla de comandosde teclas de todo que seleccionas y aplica" \
+", solo busca por nombre algo.." \
+"ejem: comment=transforma en comentario tu linea, togle comando=trnaforma tu linea en comentario o la destrasnforma")
+print("cntrl + k + cntrl + s =MOSTRARA TODOS LOS ATAJOS DE TECLADO QUE HYA Y QUE PUEDAS EDITAR")
